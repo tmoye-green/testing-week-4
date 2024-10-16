f3fcd92448a68c74a78b65f3f0c21f0b16a5a0af
monic-gao <monicgao@stanford.edu>
2024-10-16 10:48:23 -0700

    graph

---
+++ our graph.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) + geom_point() +
+  labs(x = "Engine Displacement (L)",
+       y = "Highway MPG",
+       title = "Highway MPG vs Engine Displacement", color = "Drive Type") +
+  theme_minimal()
